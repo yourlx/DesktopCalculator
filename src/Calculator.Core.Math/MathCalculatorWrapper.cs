0078e826738e483b16b315ebb262ea755691c407
yourlx <114220266+yourlx@users.noreply.github.com>
2023-08-15 18:42:08 +0700

    #8 feat: add wrapper for library

---
+++ src/Calculator.Core.Math/MathCalculatorWrapper.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Calculator.Core.Math;
+
+public class MathCalculatorWrapper : IDisposable
+{
+    private readonly IntPtr _calculator = CreateInstance();
+
+    ~MathCalculatorWrapper()
+    {
+        ReleaseUnmanagedResources();
+    }
+
+    private void ReleaseUnmanagedResources()
+    {
+        FreeInstance(_calculator);
+    }
+
+    public void Dispose()
+    {
+        ReleaseUnmanagedResources();
+        GC.SuppressFinalize(this);
+    }
+
+    public bool CheckValidAndCovert(string expression)
+    {
+        return CheckValidAndCovert(_calculator, expression);
+    }
+
+    public double Calculate(double x = 0)
+    {
+        return Calculate(_calculator, x);
+    }
+
+    [DllImport("calculatorcore")]
+    private static extern bool CheckValidAndCovert(IntPtr ptr, string expression);
+
+    [DllImport("calculatorcore")]
+    private static extern double Calculate(IntPtr ptr, double x = 0);
+
+    [DllImport("calculatorcore")]
+    private static extern IntPtr CreateInstance();
+
+    [DllImport("calculatorcore")]
+    private static extern void FreeInstance(IntPtr ptr);
+}
