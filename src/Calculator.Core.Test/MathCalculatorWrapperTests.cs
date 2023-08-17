575a28736eea0844f5bae5b0f7828a1c18bb16fe
yourlx <114220266+yourlx@users.noreply.github.com>
2023-08-17 16:55:35 +0700

    #3 init: test project

---
+++ src/Calculator.Core.Test/MathCalculatorWrapperTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Calculator.Core.Test;
+
+public class MathCalculatorWrapperTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
